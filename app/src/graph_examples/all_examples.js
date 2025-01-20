7169391bdca555f4bfa11dccabbb8094d2d20f1c
Rostislav Vrána <rostislavvrana@gmail.com>
2025-01-20 09:23:45 +0100

    feat: Added BFS test graphs

--- app/src/graph_examples/all_examples.js
+++ app/src/graph_examples/all_examples.js
@@ -3,7 +3,9 @@ import directed from "./directed.json";
 import weighted from "./weighted.json";
 import directedWeighted from "./directed_weighted.json";
 import multi from "./multi.json";
+import bfs_test from "./bfs_test.json";
+import bfs_test_directed from "./bfs_test_directed.json";
 
-const graphExamplesArray = [simple, directed, weighted, directedWeighted, multi];
+const graphExamplesArray = [bfs_test, bfs_test_directed, simple, directed, weighted, directedWeighted, multi];
 
 export default graphExamplesArray;
