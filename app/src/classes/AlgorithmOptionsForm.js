3c4c605cfd6cc5d6f0e22c956baef6f475b24c0d
Rostislav Vrána <rostislavvrana@gmail.com>
2025-02-11 14:37:56 +0100

    refactor: Using abstract class AlgorithmOptionsForm

--- app/src/classes/AlgorithmOptionsForm.js
+++ app/src/classes/AlgorithmOptionsForm.js
@@ -1,17 +1,40 @@
 
+import ErrorThrower from "./ErrorThrower";
+
 export default class AlgorithmOptionsForm {
 
-    setDefaultOptions() {
+    #options;
+    #setOptionsFunc;
+    #graph;
 
+    constructor(options, setOptionsFunc, graph) {
+        this.#options = options;
+        this.#setOptionsFunc = setOptionsFunc;
+        this.#graph = graph;
+
+        if (this.constructor === undefined) {
+            ErrorThrower.classIsAbstract("AlgorithmOptionsForm");
+        }
+
+        if (this.getDefaultOptions === undefined) {
+            ErrorThrower.methodNotImplemented("getDefaultOptions");
+        }
+
+        if (this.getComponents === undefined) {
+            ErrorThrower.methodNotImplemented("getComponents");
+        }
     }
 
     getOptions() {
-        return [];
+        return this.#options;
     }
 
+    getSetOptionsFunc() {
+        return this.#setOptionsFunc;
+    }
 
-    getComponents() {
-        return <></>;
+    getGraph() {
+        return this.#graph;
     }
 
 }
--- app/src/classes/BFSAlgorithmOptionsForm.js
+++ app/src/classes/BFSAlgorithmOptionsForm.js
@@ -1,40 +1,37 @@
 
+import AlgorithmOptionsForm from "./AlgorithmOptionsForm";
 import Form from 'react-bootstrap/Form';
 
-export default class BFSAlgorithmOptionsForm {
+export default class BFSAlgorithmOptionsForm extends AlgorithmOptionsForm {
     
-    #options;
-    #setOptionsFunc;
-    #graph;
-
-    constructor(options, setOptionsFunc, graph) {
-        this.#options = options;
-        this.#setOptionsFunc = setOptionsFunc;
-        this.#graph = graph;
-    }
-
     getDefaultOptions() {
         return [0];
     }
 
     #startingNodeChange(e) {
 
-        this.#options[0] = Number(e.target.value);
+        let options = this.getOptions();
+        const setOptionsFunc = this.getSetOptionsFunc();
 
-        this.#setOptionsFunc([...this.#options]);
+        options[0] = Number(e.target.value);
+
+        setOptionsFunc([...options]);
     }
 
     getComponents() {
 
+        const options = this.getOptions();
+        const graph = this.getGraph();
+
         let optionsHtml = [];
-        this.#graph.forEachNode((node) => {
+        graph.forEachNode((node) => {
             optionsHtml.push(<option key={node} value={node}>{node}</option>);
         });
 
         return (
             <>
                 <Form.Label className='mt-3'>Starting node</Form.Label>
-                <Form.Select value={this.#options[0]} onChange={e => this.#startingNodeChange(e)}>
+                <Form.Select value={options[0]} onChange={e => this.#startingNodeChange(e)}>
                     {optionsHtml}
                 </Form.Select>           
             </>
