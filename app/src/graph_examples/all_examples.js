c7619e2da2d1e8991db05b4c012c2ba8be1f6972
Rostislav Vrána <rostislavvrana@gmail.com>
2025-03-31 17:30:30 +0200

    feat: Created 27 graph examples

--- app/src/graph_examples/all_examples.js
+++ app/src/graph_examples/all_examples.js
@@ -1,15 +1,60 @@
 
-import algorithmTest from "./algorithm_test.json";
-import sevenBridges from "./seven_bridges_of_konigsberg.json";
-import machine from "./machine.json";
-import pragueCenterMetro from "./prague_center_metro.json"
-import flowNetwork from "./flow_network.json";
-import learn from "./learn_graph.json"
-import simple from "./simple.json"
-import components from "./components.json"
-import tarjan from "./tarjan_graph.json"
+import sevenBridges from "./seven_bridges.json";
+import cube from "./cube.json";
+import itaFig223 from "./ita_fig223.json";
+import itaFig224 from "./ita_fig224.json";
+import itaFig225 from "./ita_fig225.json";
+import itaFig226 from "./ita_fig226.json"
+import itaFig229 from "./ita_fig229.json";
+import itaFig2210 from "./ita_fig2210.json";
+import algsFig32 from "./algorithms_fig32.json";
+import algsFig36 from "./algorithms_fig36.json";
+import algsFig37 from "./algorithms_fig37.json";
+import algsFig39 from "./algorithms_fig39.json";
+import algsEx31 from "./algorithms_ex31.json";
+import algsEx32a from "./algorithms_ex32a.json";
+import algsEx32b from "./algorithms_ex32b.json";
+import algsEx34i from "./algorithms_ex34i.json";
+import algsEx34ii from "./algorithms_ex34ii.json";
+import algsEx331 from "./algorithms_ex331.json";
+import algsFig41 from "./algorithms_fig41.json";
+import skienaFig59 from "./skiena_fig59.json";
+import skienaFig511 from "./skiena_fig511.json";
+import skienaFig516 from "./skiena_fig516.json";
+import tarjanFig1 from "./tarjan_fig1.json";
+import tarjanFig2 from "./tarjan_fig2.json";
+import tarjanFig3 from "./tarjan_fig3.json";
+import pragueMetro from "./prague_metro.json";
+import summerOlympics from "./summer_olympics.json";
 
-const graphExamplesArray = [tarjan, learn, algorithmTest, simple, components,
-    sevenBridges, machine, pragueCenterMetro, flowNetwork];
+const graphExamplesArray = [
+    sevenBridges, 
+    cube, 
+    itaFig223, 
+    itaFig224,
+    itaFig225,
+    itaFig226,
+    itaFig229,
+    itaFig2210,
+    algsFig32,
+    algsFig36,
+    algsFig37,
+    algsFig39,
+    algsEx31,
+    algsEx32a,
+    algsEx32b,
+    algsEx34i,
+    algsEx34ii,
+    algsEx331,
+    algsFig41,
+    skienaFig59,
+    skienaFig511,
+    skienaFig516,
+    tarjanFig1,
+    tarjanFig2,
+    tarjanFig3,
+    pragueMetro,
+    summerOlympics
+];
 
 export default graphExamplesArray;
