d7649cccfe4b61b675d72e598e94dccfdeab78b7
Rostislav Vrána <rostislavvrana@gmail.com>
2025-08-12 18:00:07 +0200

    style: Added GitHub link

--- app/src/components/js/Title.js
+++ app/src/components/js/Title.js
@@ -3,6 +3,7 @@
 // React Bootstrap
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import { BsGithub } from "react-icons/bs";
 
 // CODE
 // Static component for showing navigation bar wih app name
@@ -11,13 +12,18 @@ export default function Title() {
         <>
             <Navbar bg='primary' data-bs-theme='dark'>
                 <Container>
-                    <Navbar.Collapse color='primary'>
+                    <Navbar.Collapse color='primary' className='justify-content-start'>
                         <Navbar.Brand>
                             Pregolya
                         </Navbar.Brand>
                         <Navbar.Text>
                             Visualizer of graph algorithms
                         </Navbar.Text>
+                    </Navbar.Collapse>
+                    <Navbar.Collapse className='justify-content-end ms-1'>
+                            <a href="https://github.com/rostcrow/pregolya">
+                                <BsGithub size={32}/>
+                            </a>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
